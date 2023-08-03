a28712522f2721941374181b98e28a1e473fc032
Parisa <drpm52@gmail.com>
2023-08-02 20:15:27 -0700

    add rel=noreferrer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer(){
             <a href="#" className= "footer-link" target="_blank"><img className="icon" src="../images/facebookIcon.png"/></a>
             <a href="#" className= "footer-link" target="_blank"><img className="icon" src="../images/instagramIcon.png"/>
             </a>
-            <a href="https://github.com/drpm52" className= "footer-link" target="_blank" rel="noreferrer"><img className="icon" src="../images/githubIcon.png"/></a>
+            <a href="https://github.com/drpm52" className= "footer-link" rel="noreferrer" target="_blank" git ><img className="icon" src="../images/githubIcon.png"/></a>
         </footer>
         </div>
     )
